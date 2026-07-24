72c39b01d1728f949a159c07d1797d5d4c56c5f4
Prem Tatkari <123146796+prem6801-code@users.noreply.github.com>
2026-07-24 23:53:37 +0530

    Create 112-PathSum

---
+++ 112-PathSum/112-PathSum.js
@@ -0,0 +1,26 @@
+// Last updated: 24/07/2026, 23:53:37
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+    if (!root) {
+        return false;
+    }
+
+    let sum = targetSum - root.val;
+    if ((!root.left && !root.right) && sum == 0) {
+        return true;
+    }
+
+    return hasPathSum(root.left, sum) || hasPathSum(root.right, sum)
+};
