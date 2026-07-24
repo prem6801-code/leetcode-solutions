8b39bf65ab01bc949beee7bc8c07a11853bc9fad
Prem Tatkari <123146796+prem6801-code@users.noreply.github.com>
2026-07-24 23:49:55 +0530

    Create 1064-SmallestIntegerDivisibleByK

---
+++ 1064-SmallestIntegerDivisibleByK/1064-SmallestIntegerDivisibleByK.js
@@ -0,0 +1,15 @@
+// Last updated: 24/07/2026, 23:49:55
+/**
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRepunitDivByK = function (k) {
+    if (k % 2 === 0 || k % 5 === 0) return -1;
+    let rem = 0;
+    for (let len = 1; len <= k; len++) {
+        rem = (rem * 10 + 1) % k;
+        if (rem === 0) return len;
+    }
+
+    return -1; 
+};
