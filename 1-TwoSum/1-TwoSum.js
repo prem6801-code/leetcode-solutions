c447ae7752a05c0bb0d1b93debeb9838f3fcae1e
Prem Tatkari <123146796+prem6801-code@users.noreply.github.com>
2026-07-24 23:55:53 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.js
@@ -0,0 +1,18 @@
+// Last updated: 24/07/2026, 23:55:52
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i=0;i<nums.length;i++){
+      if(map.has(target - nums[i])){
+        return [map.get(target-nums[i]),i];
+      }else{
+        map.set(nums[i],i)
+      }
+    }
+
+    return []
+};
