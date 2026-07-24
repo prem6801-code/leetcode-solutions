4a3b3cafd858a1974821183122a1e0cb72e36b8a
Prem Tatkari <123146796+prem6801-code@users.noreply.github.com>
2026-07-24 23:54:12 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.js
@@ -0,0 +1,21 @@
+// Last updated: 24/07/2026, 23:54:12
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let i=0;j=0;k=nums.length-1;
+    while(j<=k){
+        if(nums[j]==0){
+            [nums[i],nums[j]]=[nums[j],nums[i]];
+            i++
+            j++
+        }else if(nums[j]==2){
+            [nums[j],nums[k]]=[nums[k],nums[j]];
+            k--;
+        }
+        else{
+            j++
+        }
+    }
+};
