e366c9b47265110d0978d846bf391d70b1e1a054
Prem Tatkari <123146796+prem6801-code@users.noreply.github.com>
2026-07-24 23:53:51 +0530

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.js
@@ -0,0 +1,19 @@
+// Last updated: 24/07/2026, 23:53:50
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var isValidBST = function (root, min = -Infinity, max = Infinity) {
+    if (!root) return true;
+    if (root.val <= min || root.val >= max) return false;
+    return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+};
