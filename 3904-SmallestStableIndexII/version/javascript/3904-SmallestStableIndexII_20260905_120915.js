bceb3ec500fa6aee3a1edf9a8db67fc65a41c357
Prem Tatkari <123146796+prem6801-code@users.noreply.github.com>
2026-09-05 12:09:16 +0530

    Create 3904-SmallestStableIndexII

---
+++ 3904-SmallestStableIndexII/version/javascript/3904-SmallestStableIndexII_20260905_120915.js
@@ -0,0 +1,30 @@
+// Last updated: 05/09/2026, 12:09:15
+1/**
+2 * @param {number[]} nums
+3 * @param {number} k
+4 * @return {number}
+5 */
+6var firstStableIndex = function (nums, k) {
+7    let max = new Array(nums.length)
+8    let min = new Array(nums.length)
+9    let maxEl = nums[0]
+10    let minEl = nums[nums.length - 1]
+11    let i = 0, j = nums.length - 1;
+12    while (i < nums.length) {
+13        if (nums[i] > maxEl) {
+14            maxEl = nums[i]
+15        }
+16        if (nums[j] < minEl) {
+17            minEl = nums[j]
+18        }
+19        max[i] = maxEl
+20        min[j] = minEl
+21        i++
+22        j--
+23    }
+24
+25    for (let i = 0; i < nums.length; i++) {
+26        if ((max[i] - min[i]) <= k) return i
+27    }
+28    return -1
+29};
